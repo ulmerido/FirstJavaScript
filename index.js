a6f1b636fa2b2f66e86e74492812bfa96ac763ba
itzikya <30365522+itzikya@users.noreply.github.com>
2019-03-28 15:05:09 +0200

    trying to create a table

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log('ido is the king !!!!')
-alert('Fuck You!')
+//@ts-check
+
+let table = document.getElementById("myTable");
+const size = 10;
+
+let html = ""; 
+
+for(let i = 0; i < size; i++) {
+    html += "<tr>";
+
+    for(let j = 0; j < size; j++) {
+
+        // id is a STRING (like '02'), representing the curreny i,j cell
+       // html += `<td><a id=\'${i}${j}\'>${board[i][j]}</a></td>`;
+
+        // adding image does NOT work well on board over 4x4 (like 6x6):
+        //html += `<td><a id=\'${i}${j}\'><img src=${board[i][j]}-80.png display=${board[i][j] === ' ' ?  'none': 'block'}></a></td>`;
+        html += "<td><div id=\'${i}${j}\'></div></td>";
+
+    } // for adding each <td><a id=0> </a></td>
+    
+    html += "</tr>";
+} // for i, each <tr> block
+
+//return html;
+
+table.insertAdjacentHTML("afterbegin", html);
+
+
+
+var h = document.getElementById("myH2");
+h.insertAdjacentHTML("afterbegin", "<span style='color:red'>My span</span>");
+
