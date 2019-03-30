2e2dd33015688c6fbd60101a278c0ff0e755a14c
itzikya <30365522+itzikya@users.noreply.github.com>
2019-03-30 03:46:30 +0300

    creating board in js, adding gifs for game pieces.

--- reversi.js
+++ reversi.js
@@ -1,33 +1,30 @@
 //@ts-check
 
-let table = document.getElementById("myTable");
+var h = document.getElementById("myH2");
+h.insertAdjacentHTML("afterbegin", "<span style='color:red'>My span</span>");
+
 const size = 10;
+let table = document.getElementById("myBoard");
+let html = "";
 
-let html = ""; 
+for (let i = 0; i < size; i++) {
+  html += "<tr>";
 
-for(let i = 0; i < size; i++) {
-    html += "<tr>";
-
-    for(let j = 0; j < size; j++) {
-
-        // id is a STRING (like '02'), representing the curreny i,j cell
-       // html += `<td><a id=\'${i}${j}\'>${board[i][j]}</a></td>`;
-
-        // adding image does NOT work well on board over 4x4 (like 6x6):
-        //html += `<td><a id=\'${i}${j}\'><img src=${board[i][j]}-80.png display=${board[i][j] === ' ' ?  'none': 'block'}></a></td>`;
-        html += "<td><div id=\'${i}${j}\'></div></td>";
-
-    } // for adding each <td><a id=0> </a></td>
+  for (let j = 0; j < size; j++) {
+    // id is a string: [0][2] exc., representing the current i,j cell
     
-    html += "</tr>";
-} // for i, each <tr> block
+    // adding image does NOT work well on board over 4x4 (like 6x6):
+    //html += `<td><a id=\'${i}${j}\'><img src=${board[i][j]}-80.png display=${board[i][j] === ' ' ?  'none': 'block'}></a></td>`;
+    html += `<td id=[${i}][${j}]><img src="empty.gif" border=0 width="47" height="47"></img></td>`
+  } 
 
-//return html;
+  html += "</tr>";
+} 
 
 table.insertAdjacentHTML("afterbegin", html);
 
 
 
-var h = document.getElementById("myH2");
-h.insertAdjacentHTML("afterbegin", "<span style='color:red'>My span</span>");
+
+
 
