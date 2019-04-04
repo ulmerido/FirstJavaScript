3baf298b2a9de7f7d5acc192a2b005edc4cd92b2
itzikya <30365522+itzikya@users.noreply.github.com>
2019-04-04 17:54:10 +0300

    bug fixed

--- reversi.js
+++ reversi.js
@@ -973,14 +973,7 @@ function pad(val)
 
 function endGameAsWinner(winner)
 {
-  if (winner === ePawnType.Tie)
-  {
-    alert("There is a Tie");
-  }
-  else
-  {
-    alert(`The Winner is: The ${ winner } Player`);
-  }
+  showWinner(winner);
 
   if (winner === ePawnType.Black)
   {
@@ -1053,6 +1046,36 @@ function technicalVictory()
   else if (g_PlayerTurnType === ePawnType.Black)
   {
     endGameAsWinner(ePawnType.White);
+  }
+}
+
+var modal = document.getElementById('myModal');
+var modalText = document.getElementById('myModalText');
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+function showWinner(winner)
+{
+  modal.style.display = "block";
+  if (winner === ePawnType.Tie)
+  {
+    modalText.innerHTML = "There is a Tie";
+  }
+  else
+  {
+    modalText.innerHTML = `The Winner is: The ${winner} Player`;
+  }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
 
